03872962c147b69adff474f444434d08ade4ae0c
vigneshwaran <vigneshwaran.p@fundsindia.com>
2024-09-12 23:35:18 +0530

    footer added

--- Angular/src/app/pages/home/home.component.ts
+++ Angular/src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { BannerComponent } from '../../components/banner/banner.component';
 import { AboutUsComponent } from '../../components/about-us/about-us.component';
 import { ShortMenuComponent } from '../../components/short-menu/short-menu.component';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent, AboutUsComponent, ShortMenuComponent, CarouselComponent],
+  imports: [BannerComponent, AboutUsComponent, ShortMenuComponent, CarouselComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
